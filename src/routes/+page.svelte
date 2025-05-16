4687af2b93dcc2f3261e90b4fb19cfac6eded8a0
Mariano <longo.mariano93@yahoo.it>
2025-05-16 18:42:54 +0200

    feat: implement language redirection and update Minesweeper card in Italian and English pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,21 @@
 	let cards: HTMLDivElement;
 	let interval: ReturnType<typeof setInterval>;
 
+	if (typeof window !== 'undefined') {
+		const lang = navigator.language;
+		const path = window.location.pathname;
+		if (lang && lang.startsWith('it')) {
+			// Sei già sulla pagina italiana o ci resti
+			if (path !== '/') {
+				window.location.href = '/';
+			}
+		} else {
+			if (path !== '/english') {
+				window.location.href = '/english';
+			}
+		}
+	}
+
 	onMount(() => {
 		interval = setInterval(() => {
 			if (cards && cards.children.length > 0) {
@@ -24,205 +39,255 @@
 	});
 </script>
 
-<div class="container funnel-sans-regular">
-    <div class="top-icons">
-        <a href="/english" aria-label="English" title="English">
-            <img src="/gb.png" alt="English Flag" class="flag" width="32" height="32" />
-        </a>
-    </div>
+<div class="funnel-sans-regular container">
+	<div class="top-icons">
+		<a href="/english" aria-label="English" title="English">
+			<img src="/gb.png" alt="English Flag" class="flag" width="32" height="32" />
+		</a>
+	</div>
 	<div class="block-grad block">
 		<h1>Salve, il mio nome è Mariano L.</h1>
-        <h2>Sono un laureato in Informatica, appassionato di sviluppo software e di tutto ciò che riguarda il mondo tech.</h2>
-        <div class="block-links">
-            <a href="https://www.linkedin.com/in/marianolongo" class="block-link" target="_blank" rel="noopener">
-                <img src="/linkedin.png" alt="linkedin" width="32" height="32" /></a>
-            <a href="https://github.com/Greed71" class="block-link" target="_blank" rel="noopener">
-                <img src="/git.png" alt="Git" width="32" height="32" /></a>
-        </div>
+		<h2>
+			Sono un laureato in Informatica, appassionato di sviluppo software e di tutto ciò che riguarda
+			il mondo tech.
+		</h2>
+		<div class="block-links">
+			<a
+				href="https://www.linkedin.com/in/marianolongo"
+				class="block-link"
+				target="_blank"
+				rel="noopener"
+			>
+				<img src="/linkedin.png" alt="linkedin" width="32" height="32" /></a
+			>
+			<a href="https://github.com/Greed71" class="block-link" target="_blank" rel="noopener">
+				<img src="/git.png" alt="Git" width="32" height="32" /></a
+			>
+		</div>
 	</div>
 	<div class="cards" bind:this={cards}>
-		<div class="card card-rosso">Contenuto 1</div>
+		<div class="card card-rosso card-minesweeper">
+			<img src="/mine.svg" alt="Minesweeper" width="32" height="32" class="card-img-top" />
+			<span class="card-title">Minesweeper</span>
+			<div class="card-buttons">
+                <a href="https://minesweeper-two-drab.vercel.app/" target="_blank" rel="noopener">
+					<img
+						src="/link-s.png"
+                        alt="Link"
+						width="32"
+						height="32"
+					/>
+				</a>
+				<a href="https://github.com/Greed71/Minesweeper" target="_blank" rel="noopener">
+					<img
+						src="/git.png"
+                        alt="Git"
+						width="32"
+						height="32"
+					/>
+				</a>
+			</div>
+		</div>
 		<div class="card card-blu">Contenuto 2</div>
 		<div class="card card-giallo">Contenuto 3</div>
 		<div class="card card-verde">Contenuto 4</div>
 		<div class="card card-viola">Contenuto 5</div>
-		<!-- Aggiungi altri quadrettoni qui -->
 	</div>
-    <div class="footer-link">
-        <a href="https://github.com/Greed71/Portfolio" aria-label="Git">
-            <img src="/git.png" alt="GitHub Logo" class="github-logo" width="32" height="32" />
-            Visualizza il codice sorgente su GitHub
-        </a>
-    </div>
+	<div class="footer-link">
+		<a href="https://github.com/Greed71/Portfolio" aria-label="Git">
+			<img src="/git.png" alt="GitHub Logo" class="github-logo" width="32" height="32" />
+			Visualizza il codice sorgente su GitHub
+		</a>
+	</div>
 </div>
 
 <style>
-    :global(body) {
-        margin: 0;
-        padding: 0;
-        min-height: 100vh;
-        background-color: #006d5b; 
-        font-family: sans-serif;
-    }
+	:global(body) {
+		margin: 0;
+		padding: 0;
+		min-height: 100vh;
+		background-color: #006d5b;
+		font-family: sans-serif;
+	}
 
-    h1 {
-        font-size: 2rem;
-        font-weight: 700;
-        margin: 0;
-        padding: 0;
-    }
+	h1 {
+		font-size: 2rem;
+		font-weight: 700;
+		margin: 0;
+		padding: 0;
+	}
 
-    h2 {
-        text-align: left;
-        font-size: 1.4rem;
-        font-weight: 400;
-        margin: 0;
-        padding: 0;
-    }
+	h2 {
+		text-align: left;
+		font-size: 1.4rem;
+		font-weight: 400;
+		margin: 0;
+		padding: 0;
+	}
 
-    .container {
-        min-height: 100vh;
-        display: flex;
-        flex-direction: column;
-        align-items: flex-end; 
-        justify-content: center; 
-        color: white;
-        position: relative;
-        z-index: 1;
-    }
+	.container {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+		align-items: flex-end;
+		justify-content: center;
+		color: white;
+		position: relative;
+		z-index: 1;
+	}
 
-    .block {
-        width: 100%;
-        min-height: 300px;
-        max-width: 1000px;
-        background: rgba(255, 255, 255, 0.1);
-        border-radius: 1rem;
-        box-shadow: 0 4px 16px rgba(0, 0, 0, 0.2);
-        padding: 2rem;
-        color: white;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: flex-start;
-        text-align: center;
-        font-size: 1.5rem;
-        margin-top: 0;
-        margin-bottom: 2rem;
-        position: relative;
-    }
+	.block {
+		width: 100%;
+		min-height: 300px;
+		max-width: 1000px;
+		background: rgba(255, 255, 255, 0.1);
+		border-radius: 1rem;
+		box-shadow: 0 4px 16px rgba(0, 0, 0, 0.2);
+		padding: 2rem;
+		color: white;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: flex-start;
+		text-align: center;
+		font-size: 1.5rem;
+		margin-top: 0;
+		margin-bottom: 2rem;
+		position: relative;
+	}
 
-    .block-link {
-        margin-top: auto;
-        margin-bottom: 0;
-        align-self: center;
-        color: #fff;
-        padding: 0.5rem 0.2rem; /* padding orizzontale ridotto */
-        text-decoration: none;
-        font-weight: bold;
-    }
+	.block-link {
+		margin-top: auto;
+		margin-bottom: 0;
+		align-self: center;
+		color: #fff;
+		padding: 0.5rem 0.2rem;
+		text-decoration: none;
+		font-weight: bold;
+	}
 
-    .block-links {
-        display: flex;
-        gap: 0rem;         /* nessuno spazio tra i link */
-        margin-top: auto;
-        margin-bottom: 0;
-        justify-content: center;
-        width: 100%;
-    }
+	.block-links {
+		display: flex;
+		gap: 0rem;
+		margin-top: auto;
+		margin-bottom: 0;
+		justify-content: center;
+		width: 100%;
+	}
 
-    .cards {
-        display: flex;
-        gap: 2rem;
-        overflow: hidden;
-        width: 100%;
-        max-width: 1000px;
-        margin-top: 0;
-        flex-wrap: nowrap;
-        justify-content: flex-start;
-        position: relative;
-        padding-top: 12px;
-        padding-bottom: 12px;
-        padding-right: 18px;
-        padding-left: 18px;
-    }
+	.cards {
+		display: flex;
+		gap: 2rem;
+		overflow: hidden;
+		width: 100%;
+		max-width: 1000px;
+		margin-top: 0;
+		flex-wrap: nowrap;
+		justify-content: flex-start;
+		position: relative;
+		padding-top: 12px;
+		padding-bottom: 12px;
+		padding-right: 18px;
+		padding-left: 18px;
+	}
 
-    .card {
-        background: rgba(255, 255, 255, 0.1);
-        border-radius: 1rem;
-        box-shadow: 0 4px 16px rgba(0, 0, 0, 0.2);
-        padding: 2rem;
-        min-width: 300px;
-        min-height: 180px;
-        max-width: 300px;
-        color: white;
-        display: flex;
-        align-items: flex-start;  
-        justify-content: flex-start;  
-        text-align: left;             
-        font-size: 1.2rem;
-        transition: margin-left 0.4s;
-        flex-shrink: 0;
-    }
+	.card {
+		background: rgba(255, 255, 255, 0.1);
+		border-radius: 1rem;
+		box-shadow: 0 4px 16px rgba(0, 0, 0, 0.2);
+		padding: 2rem;
+		min-width: 300px;
+		min-height: 180px;
+		max-width: 300px;
+		color: white;
+		display: flex;
+		align-items: flex-start;
+		justify-content: flex-start;
+		text-align: left;
+		font-size: 1.2rem;
+		transition: margin-left 0.4s;
+		flex-shrink: 0;
+	}
 
-    .block-grad {
-        background: linear-gradient(135deg, #3498db 0%, #9b59b6 100%);
-    }
+	.block-grad {
+		background: linear-gradient(135deg, #3498db 0%, #9b59b6 100%);
+	}
 
-    .card-rosso {
-        background: #e74c3c;
-    }
-    .card-blu {
-        background: #4e44db;
-    }
-    .card-giallo {
-        background: #f1c40f;
-    }
-    .card-verde {
-        background: #2ecc71;
-    }
-    .card-viola {
-        background: #9b59b6;
-    }
+	.card-rosso {
+		background: #e74c3c;
+	}
+	.card-blu {
+		background: #4e44db;
+	}
+	.card-giallo {
+		background: #f1c40f;
+	}
+	.card-verde {
+		background: #2ecc71;
+	}
+	.card-viola {
+		background: #9b59b6;
+	}
 
-    .funnel-sans-regular {
-        font-family: "Funnel Sans", sans-serif;
-        font-optical-sizing: auto;
-        font-weight: 300;
-        font-style: normal;
-    }
+	.funnel-sans-regular {
+		font-family: 'Funnel Sans', sans-serif;
+		font-optical-sizing: auto;
+		font-weight: 300;
+		font-style: normal;
+	}
 
-    .top-icons {
-        width: 100%;
-        max-width: 1000px;
-        display: flex;
-        justify-content: flex-end;
-        align-items: center;
-        margin-bottom: 0; 
-        gap: 0.5rem;
-    }
+	.top-icons {
+		width: 100%;
+		max-width: 1000px;
+		display: flex;
+		justify-content: flex-end;
+		align-items: center;
+		margin-bottom: 0;
+		gap: 0.5rem;
+	}
 
-    .flag {
-        font-size: 2rem;
-        user-select: none;
-    }
+	.flag {
+		font-size: 2rem;
+		user-select: none;
+	}
 
-    .footer-link {
-        width: 100%;
-        position: fixed;
-        bottom: 20px;
-        left: 0;
-        display: flex;
-        justify-content: center;
-        z-index: 10;
-    }
+	.footer-link {
+		width: 100%;
+		position: fixed;
+		bottom: 20px;
+		left: 0;
+		display: flex;
+		justify-content: center;
+		z-index: 10;
+	}
 
-    .footer-link a {
-        color: rgb(0, 0, 0);
-        text-decoration: none;
-        font-size: 1rem;
-        font-weight: bold;
-        display: inline-flex;
-        align-items: center;
-        gap: 0.5rem;
-    }
+	.footer-link a {
+		color: rgb(0, 0, 0);
+		text-decoration: none;
+		font-size: 1rem;
+		font-weight: bold;
+		display: inline-flex;
+		align-items: center;
+		gap: 0.5rem;
+	}
+
+	.card-minesweeper {
+		position: relative;
+		overflow: hidden;
+	}
+	.card-img-top {
+		position: relative;
+		z-index: 2;
+	}
+
+	.card-buttons {
+		position: absolute;
+		bottom: 1rem;
+		left: 0;
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		gap: 0.5rem;
+		z-index: 3;
+	}
 </style>
